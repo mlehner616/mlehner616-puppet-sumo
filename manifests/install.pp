b1d4733c69610bca7d9bfbdab032e2176008ee75
Brandon Weeks <brandon.weeks@gree.net>
2014-12-09 13:36:21 -0800

    Append ${sumo::install_script_dir} to qualify ${sumo::install_script}

--- manifests/install.pp
+++ manifests/install.pp
@@ -5,16 +5,16 @@ class sumo::install inherits sumo {
   exec { 'Install sumo from sumologic.com':
     command => $sumo::get_installer_cmd,
     cwd     => $sumo::install_script_dir,
-    creates => $sumo::install_script,
+    creates => "${sumo::install_script_dir}/${sumo::install_script}",
   }->
-  file { $sumo::install_script:
+  file { "${sumo::install_script_dir}/${sumo::install_script}":
     ensure => file,
     mode   => '0100',
     owner  => 'root',
     group  => 'root',
   }->
   exec { 'Install sumo from local':
-    command => "${sumo::install_script} -q",
+    command => "${sumo::install_script_dir}/${sumo::install_script} -q",
     creates => $sumo::install_dir,
   }
 
